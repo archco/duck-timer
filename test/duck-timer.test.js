ed2fd75c0b1b85102f168c751788f037127322a0
archco <archcoster@gmail.com>
2018-01-23 11:08:52 +0900

    Update tests.

--- test/duck-timer.test.js
+++ test/duck-timer.test.js
@@ -1,111 +1,123 @@
-describe('DuckTimer', () => {
-  describe('constructor', () => {
-    it('can constructed.', () => {
-      let timer = new DuckTimer();
+describe('DuckTimer', function () {
+  describe('constructor', function () {
+    it('can constructed.', function () {
+      var timer = new DuckTimer();
       expect(timer).to.be.instanceOf(DuckTimer);
     });
   });
 
-  describe('options', () => {
-    it('setTime: set time value(milliseconds).', () => {
-      let timer = new DuckTimer({ setTime: 1200 });
+  describe('options', function () {
+    it('setTime: set time value(milliseconds).', function () {
+      var timer = new DuckTimer({ setTime: 1200 });
       expect(timer.time).to.equal(1200);
     });
 
-    it('interval: milliseconds value for callback invoke.', () => {
-      let timer = new DuckTimer({ interval: 1000 });
+    it('interval: milliseconds value for callback invoke.', function () {
+      var timer = new DuckTimer({ interval: 1000 });
       expect(timer.option.interval).to.equal(1000);
     });
 
-    it('onInterval: callback function.', () => {
-      let timer = new DuckTimer({ onInterval: res => console.log(res) });
-      let listeners = timer.getEventEmitter().listeners('interval');
+    it('onInterval: callback function.', function () {
+      var timer = new DuckTimer({
+        onInterval: function (res) {
+          console.log(res);
+        }
+      });
+      var listeners = timer.getEventEmitter().listeners('interval');
 
       expect(timer.option.onInterval).to.be.a('function');
       expect(listeners.length).to.equal(1);
     });
 
-    it('timeout: Set timeout time(milliseconds).', () => {
-      let timer = new DuckTimer({ timeout: 1000 });
+    it('timeout: Set timeout time(milliseconds).', function () {
+      var timer = new DuckTimer({ timeout: 1000 });
       expect(timer.option.timeout).to.equal(1000);
     });
 
-    it('onTimeout: callback function that invoke when timeout done or countdown finished.', () => {
-      let timer = new DuckTimer({ onTimeout: res => console.log(res) });
-      let listeners = timer.getEventEmitter().listeners('timeout');
+    it('onTimeout: callback function that invoke when timeout done or countdown finished.', function () {
+      var timer = new DuckTimer({
+        onTimeout: function (res) {
+          console.log(res);
+        }
+      });
+      var listeners = timer.getEventEmitter().listeners('timeout');
       expect(timer.option.onTimeout).to.be.a('function');
       expect(listeners.length).to.equal(1);
     });
 
-    it('countdownDate: set endDate value to "timeClock"', () => {
-      let timer = new DuckTimer({ countdownDate: '2010-11-01' });
-      let endDate = timer.getClock().endDate;
+    it('countdownDate: set endDate value to "timeClock"', function () {
+      var timer = new DuckTimer({ countdownDate: '2010-11-01' });
+      var endDate = timer.getClock().endDate;
       expect(endDate).to.be.instanceOf(Date);
       expect(endDate.getFullYear()).to.equal(2010);
     });
   });
 
-  describe('#getClock', () => {
-    it('Return instance of TimeClock.', () => {
-      let timer = new DuckTimer({ setTime: 3000 });
+  describe('#getClock', function () {
+    it('Return instance of TimeClock.', function () {
+      var timer = new DuckTimer({ setTime: 3000 });
       expect(timer.getClock()).to.be.instanceOf(TimeClock);
       expect(timer.getClock().seconds).to.equal(3);
     });
   });
 
-  describe('#getEventEmitter', () => {
-    it('Return instance of eventemitter3', () => {
-      let timer = new DuckTimer();
-      timer.setInterval(1000, res => console.log(res));
+  describe('#getEventEmitter', function () {
+    it('Return instance of eventemitter3', function () {
+      var timer = new DuckTimer();
+      timer.setInterval(1000, function (res) {
+        console.log(res);
+      });
       expect(timer.getEventEmitter()).to.be.an('object');
       expect(timer.getEventEmitter().listeners('interval')).is.not.empty;
     });
   });
 
-  describe('Feature: Interval callback', () => {
-    let clock;
+  describe('Feature: Interval callback', function () {
+    var clock;
 
-    beforeEach(() => {
+    beforeEach(function () {
       clock = sinon.useFakeTimers();
     });
 
-    afterEach(() => {
+    afterEach(function () {
       clock.restore();
     });
 
-    it('interval callback\'s response is not empty.', () => {
-      let timer = new DuckTimer();
-      timer.setInterval(1000, res => {
+    it('interval callback\'s response is not empty.', function () {
+      var timer = new DuckTimer();
+      timer.setInterval(1000, function (res) {
         expect(res).is.not.empty;
         expect(res).to.be.instanceOf(TimeClock);
       }).start();
       clock.tick(1000);
     });
 
-    it('If interval set 100 milliseconds and clock passed 1 sec, then callback called 10 times.', () => { //jscs:ignore
-      let timer = new DuckTimer({ interval: 100 });
-      let count = 0;
-      timer.onInterval(() => count++).start();
+    it('If interval set 100 milliseconds and clock passed 1 sec, then callback called 10 times.', function () { //jscs:ignore
+      var timer = new DuckTimer({ interval: 100 });
+      var count = 0;
+      timer.onInterval(function () {
+        count++;
+      }).start();
       clock.tick(1000);
       expect(count).to.equal(10);
     });
   });
 
-  describe('Feature: Timeout callback', () => {
-    let clock;
+  describe('Feature: Timeout callback', function () {
+    var clock;
 
-    beforeEach(() => {
+    beforeEach(function () {
       clock = sinon.useFakeTimers();
     });
 
-    afterEach(() => {
+    afterEach(function () {
       clock.restore();
     });
 
-    it('If timeout time is come, then "timeout" event will occur.', () => {
-      let timer = new DuckTimer({ timeout: 3000 });
-      let occurred = false;
-      timer.onTimeout(res => {
+    it('If timeout time is come, then "timeout" event will occur.', function () {
+      var timer = new DuckTimer({ timeout: 3000 });
+      var occurred = false;
+      timer.onTimeout(function (res) {
         expect(res).to.be.instanceOf(TimeClock);
         occurred = true;
       }).start();
@@ -119,9 +131,9 @@ describe('DuckTimer', () => {
       expect(occurred).to.be.true;
     });
 
-    it('In clock, "timeout" and "remain" properties defined automatically.', () => {
-      let timer = new DuckTimer();
-      let result;
+    it('In clock, "timeout" and "remain" properties defined automatically.', function () {
+      var timer = new DuckTimer();
+      var result;
       timer.setTimeout(3000).start();
       expect(timer.getClock().timeout).to.be.instanceOf(TimeClock);
       expect(timer.getClock().remain.time).to.equal(3000);
@@ -130,29 +142,30 @@ describe('DuckTimer', () => {
     });
   });
 
-  describe('Feature: Stopwatch', () => {
-    let clock;
-    let timer;
+  describe('Feature: Stopwatch', function () {
+    var clock;
+    var timer;
 
-    beforeEach(() => {
+    beforeEach(function () {
       clock = sinon.useFakeTimers();
       timer = new DuckTimer({ interval: 1000, tick: 10 });
     });
 
-    afterEach(() => {
+    afterEach(function () {
       clock.restore();
     });
 
-    describe('#start', () => {
-      it('call start(), then time running.', () => {
-        timer.onInterval(res => expect(res.time).to.equal(1000))
-          .start();
+    describe('#start', function () {
+      it('call start(), then time running.', function () {
+        timer.onInterval(function (res) {
+          expect(res.time).to.equal(1000);
+        }).start();
         clock.tick(1000);
       });
     });
 
-    describe('#stop', () => {
-      it('If stop during time is running, then time is stop.', () => {
+    describe('#stop', function () {
+      it('If stop during time is running, then time is stop.', function () {
         timer.start();
         clock.tick(460);
         expect(timer.time).to.equal(460);
@@ -162,8 +175,8 @@ describe('DuckTimer', () => {
       });
     });
 
-    describe('#reset', () => {
-      it('If reset, then time set 0.', () => {
+    describe('#reset', function () {
+      it('If reset, then time set 0.', function () {
         timer.start();
         clock.tick(400);
         expect(timer.time).to.equal(400);
@@ -173,26 +186,26 @@ describe('DuckTimer', () => {
     });
   });
 
-  describe('Feature: Countdown', () => {
-    let clock;
-    let timer;
+  describe('Feature: Countdown', function () {
+    var clock;
+    var timer;
 
-    beforeEach(() => {
+    beforeEach(function () {
       clock = sinon.useFakeTimers();
       timer = new DuckTimer();
     });
 
-    afterEach(() => {
+    afterEach(function () {
       clock.restore();
     });
 
-    describe('#setCountdown', () => {
-      it('Second argument is optional.', () => {
-        let m = 5;
-        let date = new Date();
+    describe('#setCountdown', function () {
+      it('Second argument is optional.', function () {
+        var m = 5;
+        var date = new Date();
         date.setMinutes(date.getMinutes() + m);
         timer.setCountdown(date)
-          .setInterval(60000, res => {
+          .setInterval(60000, function (res) {
             m--;
             expect(res.remain.minutes).to.equal(m);
           }).start();
@@ -201,11 +214,11 @@ describe('DuckTimer', () => {
       });
     });
 
-    describe('Countdown test.', () => {
-      it('5 minutes countdown test.', () => {
-        let m = 5;
+    describe('Countdown test.', function () {
+      it('5 minutes countdown test.', function () {
+        var m = 5;
         timer.setCountdown('2017-11-03 00:05:00', '2017-11-03 00:00:00')
-          .setInterval(60000, res => {
+          .setInterval(60000, function (res) {
             m--;
             expect(res.remain.minutes).to.equal(m);
           }).start();
@@ -213,10 +226,10 @@ describe('DuckTimer', () => {
         expect(m).to.equal(0);
       });
 
-      it('When countdown finished, "timeout" event is occurred.', () => {
-        let occurred = false;
+      it('When countdown finished, "timeout" event is occurred.', function () {
+        var occurred = false;
         timer.setCountdown('2017-11-03 00:05:00', '2017-11-03 00:00:00')
-          .onTimeout(() => {
+          .onTimeout(function () {
             occurred = true;
           }).start();
         clock.tick(300000);
@@ -224,16 +237,16 @@ describe('DuckTimer', () => {
       });
     });
 
-    describe('Auto delay test.', () => {
-      it('Real time = delayed time + time', () => {
-        let interval = 1000;
-        let result;
+    describe('Auto delay test.', function () {
+      it('Real time = delayed time + time', function () {
+        var interval = 1000;
+        var result;
         timer.setOption({ enableAutoDelay: true })
           .setCountdown('2000-01-01 00:05:00', '2000-01-01 00:00:00.250')
-          .setInterval(interval, res => {
+          .setInterval(interval, function (res) {
             expect(res.remain.time % interval).to.equal(0);
           })
-          .onTimeout(res => {
+          .onTimeout(function (res) {
             result = res;
           })
           .start();
@@ -243,30 +256,30 @@ describe('DuckTimer', () => {
     });
   });
 
-  describe('Feature: delay', () => {
-    let clock;
-    let timer;
+  describe('Feature: delay', function () {
+    var clock;
+    var timer;
 
-    beforeEach(() => {
+    beforeEach(function () {
       clock = sinon.useFakeTimers();
       timer = new DuckTimer();
     });
 
-    afterEach(() => {
+    afterEach(function () {
       clock.restore();
     });
 
-    it('can set option: enableAutoDelay (default = true)', () => {
+    it('can set option: enableAutoDelay (default = true)', function () {
       expect(timer.option.enableAutoDelay).to.be.true;
       timer.setOption({ enableAutoDelay: false });
       expect(timer.option.enableAutoDelay).to.be.false;
     });
 
-    describe('#setDelay', () => {
-      it('First argument is delay time.', () => {
-        let time;
-        let delayed;
-        timer.setTimeout(1000, res => {
+    describe('#setDelay', function () {
+      it('First argument is delay time.', function () {
+        var time;
+        var delayed;
+        timer.setTimeout(1000, function (res) {
           time = res.time;
           delayed = res.delayed;
         }).setDelay(1000).start();
@@ -275,10 +288,10 @@ describe('DuckTimer', () => {
         expect(delayed).to.equal(1000);
       });
 
-      it('Second argument is callback when delay done. (optional)', () => {
-        let time;
-        let delayed;
-        timer.setTimeout(1000).setDelay(1000, res => {
+      it('Second argument is callback when delay done. (optional)', function () {
+        var time;
+        var delayed;
+        timer.setTimeout(1000).setDelay(1000, function (res) {
           time = res.time;
           delayed = res.delayed;
         }).start();
--- test/time-clock.test.js
+++ test/time-clock.test.js
@@ -1,89 +1,89 @@
-describe('TimeClock', () => {
-  describe('constructor', () => {
-    it('First argument is milliseconds time.', () => {
-      let clock = new TimeClock(1000);
+describe('TimeClock', function () {
+  describe('constructor', function () {
+    it('First argument is milliseconds time.', function () {
+      var clock = new TimeClock(1000);
       expect(clock).to.be.instanceOf(TimeClock);
       expect(clock.time).to.equal(1000);
     });
   });
 
-  describe('getters', () => {
-    it('seconds: 3000-3999 milliseconds equal to 3 seconds.', () => {
-      let clock = new TimeClock(3000);
+  describe('getters', function () {
+    it('seconds: 3000-3999 milliseconds equal to 3 seconds.', function () {
+      var clock = new TimeClock(3000);
       expect(clock.seconds).to.equal(3);
       clock.time = 3600;
       expect(clock.seconds).to.equal(3);
     });
 
-    it('minutes: 300,000 milliseconds equal to 5 minutes.', () => {
-      let clock = new TimeClock(300000);
+    it('minutes: 300,000 milliseconds equal to 5 minutes.', function () {
+      var clock = new TimeClock(300000);
       expect(clock.minutes).to.equal(5);
     });
 
-    it('hours: 21,600,000 milliseconds equal to 6 hours.', () => {
-      let clock = new TimeClock(21600000);
+    it('hours: 21,600,000 milliseconds equal to 6 hours.', function () {
+      var clock = new TimeClock(21600000);
       expect(clock.hours).to.equal(6);
     });
 
-    it('days: 604,800,000 milliseconds equal to 7 days.', () => {
-      let clock = new TimeClock(604800000);
+    it('days: 604,800,000 milliseconds equal to 7 days.', function () {
+      var clock = new TimeClock(604800000);
       expect(clock.days).to.equal(7);
     });
   });
 
-  describe('#toData', () => {
-    let clock = new TimeClock(8764500000);
+  describe('#toData', function () {
+    var clock = new TimeClock(8764500000);
 
-    it('Returns time value to data object.', () => {
+    it('Returns time value to data object.', function () {
       expect(clock.toData()).to.be.an('object');
     });
 
-    it("Data have keys: ['day', 'hour', 'min', 'sec', 'ms']", () => {
+    it("Data have keys: ['day', 'hour', 'min', 'sec', 'ms']", function () {
       expect(clock.toData()).to.have.all.keys(['day', 'hour', 'min', 'sec', 'ms']);
     });
   });
 
-  describe('#toTimeString', () => {
-    it('Return time to string as simple format.', () => {
-      let clock = new TimeClock(9000000);
+  describe('#toTimeString', function () {
+    it('Return time to string as simple format.', function () {
+      var clock = new TimeClock(9000000);
       expect(clock.toTimeString()).to.be.a('string');
     });
   });
 
-  describe('#setDistance', () => {
-    let clock;
+  describe('#setDistance', function () {
+    var clock;
 
-    before(() => {
+    before(function () {
       clock = new TimeClock();
       clock.setDistance('2017-11-01', '2017-11-30 21:00:00');
     });
 
-    it('startDate and endDate properties are instanceof Date.', () => {
+    it('startDate and endDate properties are instanceof Date.', function () {
       expect(clock.startDate).to.instanceOf(Date);
       expect(clock.endDate).to.instanceOf(Date);
     });
 
-    it('distance will returns time between startDate and endDate as instance of TimeClock.', () => {
+    it('distance will returns time between startDate and endDate as instance of TimeClock.', function () {
       expect(clock.distance).to.instanceOf(TimeClock);
       expect(clock.distance.time).to.be.a('number');
     });
 
-    it('remain property will returns as instance of TimeClock.', () => {
+    it('remain property will returns as instance of TimeClock.', function () {
       expect(clock.remain).to.instanceOf(TimeClock);
       expect(clock.remain.time).to.be.a('number');
     });
   });
 
-  describe('.timeout', () => {
-    let clock = new TimeClock();
+  describe('.timeout', function () {
+    var clock = new TimeClock();
     clock.setTimeout(3000);
 
-    it('Getter and setter will timeout convert to TimeClock.', () => {
+    it('Getter and setter will timeout convert to TimeClock.', function () {
       expect(clock.timeout).to.be.instanceOf(TimeClock);
       expect(clock.timeout.time).to.equal(3000);
     });
 
-    it('Getter "remain" will be available.', () => {
+    it('Getter "remain" will be available.', function () {
       expect(clock.remain).to.be.instanceOf(TimeClock);
       expect(clock.remain.time).to.equal(3000);
     });
